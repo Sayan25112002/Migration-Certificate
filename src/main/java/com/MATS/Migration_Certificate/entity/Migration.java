a83b60971e802c1b9327b54616f9b09dd7a53aef
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-10 11:49:31 +0530

    MigrationCertificate.jrxml modified

---
+++ src/main/java/com/MATS/Migration_Certificate/entity/Migration.java
@@ -0,0 +1,53 @@
+package com.MATS.Migration_Certificate.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Migration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY])
+    private Long id;
+
+    private String image;
+
+    private String date;
+
+    private String name;
+
+    private String fatherName;
+
+    private String motherName;
+
+    private String applicationNumber;
+
+    private String enrollmentNumber;
+
+    private String course;
+
+    private String year;
+
+    private String naam;
+
+    private String papaKaNaam;
+
+    private String maaKaNaam;
+
+    private String aavedanSankhya;
+
+    private String namanthinSankhya;
+
+    private String saal;
+
+    private String shiksha;
+
+
+
+}
