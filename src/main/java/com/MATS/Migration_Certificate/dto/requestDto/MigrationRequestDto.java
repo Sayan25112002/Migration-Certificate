5f2dce188b65cf04782c752af0928444b6e9a0ea
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-10 11:56:16 +0530

    MigrationRequestDto Added

---
+++ src/main/java/com/MATS/Migration_Certificate/dto/requestDto/MigrationRequestDto.java
@@ -0,0 +1,54 @@
+package com.MATS.Migration_Certificate.dto.requestDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MigrationRequestDto {
+
+    private String image;
+
+    private String date;
+
+    private String name;
+
+    private String fatherName;
+
+    private String motherName;
+
+    private String applicationNumber;
+
+    private String enrollmentNumber;
+
+    private String course;
+
+    private String year;
+
+    private String naam;
+
+    private String papaKaNaam;
+
+    private String maaKaNaam;
+
+    private String aavedanSankhya;
+
+    private String namanthinSankhya;
+
+    private String saal;
+
+    private String shiksha;
+
+    private String controllerOfExamination;
+
+    private String registrar;
+
+    private String verifiedDate;
+
+    private String certificateDate;
+
+}
