0ed1aa2302fd853c3bc630c29007a79508a43358
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-10 11:32:01 +0530

    Migration Cerificate.jrxml added

---
+++ src/main/java/com/MATS/Migration_Certificate/MigrationCertificateApplication.java
@@ -0,0 +1,13 @@
+package com.MATS.Migration_Certificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationCertificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationCertificateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MATS/Migration_Certificate/MigrationCertificateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MATS.Migration_Certificate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationCertificateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
