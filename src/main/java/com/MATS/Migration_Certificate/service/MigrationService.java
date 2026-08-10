b8de56db8d5a6b828e8b6b7df3af629cd5efc053
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-10 12:37:32 +0530

    MigrationServiceImpl Added and MigrationService Modified

--- src/main/java/com/MATS/Migration_Certificate/service/MigrationService.java
+++ src/main/java/com/MATS/Migration_Certificate/service/MigrationService.java
@@ -2,13 +2,16 @@ package com.MATS.Migration_Certificate.service;
 
 import com.MATS.Migration_Certificate.dto.requestDto.MigrationRequestDto;
 import com.MATS.Migration_Certificate.dto.responseDto.MigrationResponseDto;
+import net.sf.jasperreports.engine.JRException;
+
+import java.io.IOException;
 
 public interface MigrationService {
 
-    MigrationResponseDto createMigration(MigrationRequestDto migrationRequestDto);
+    MigrationResponseDto createMigration(MigrationRequestDto migrationRequestDto) throws IOException;
 
     MigrationResponseDto getMigration(Long id);
 
-    byte[] generateMigrationCertificate(Long id);
+    byte[] generateMigrationCertificate(Long id) throws JRException;
 
 }
--- src/main/java/com/MATS/Migration_Certificate/service/implementation/MigrationServiceImpl.java
+++ src/main/java/com/MATS/Migration_Certificate/service/implementation/MigrationServiceImpl.java
@@ -6,14 +6,21 @@ import com.MATS.Migration_Certificate.entity.Migration;
 import com.MATS.Migration_Certificate.mapper.MigrationMapper;
 import com.MATS.Migration_Certificate.repository.MigrationRepository;
 import com.MATS.Migration_Certificate.service.MigrationService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -23,19 +30,39 @@ public class MigrationServiceImpl implements MigrationService {
     private final MigrationMapper migrationMapper;
 
     @Override
-    public MigrationResponseDto createMigration(MigrationRequestDto migrationRequestDto) {
+    public MigrationResponseDto createMigration(MigrationRequestDto migrationRequestDto) throws IOException {
         Migration migration = migrationMapper.toMigration(migrationRequestDto);
         migration.setImage(saveFile(migrationRequestDto.getImageFile()));
+        Migration savedMigration = migrationRepository.save(migration);
+        return migrationMapper.toMigrationResponseDto(savedMigration);
     }
 
     @Override
     public MigrationResponseDto getMigration(Long id) {
-        return null;
+        Migration migration = migrationRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Migration not found"));
+        return migrationMapper.toMigrationResponseDto(migration);
     }
 
     @Override
-    public byte[] generateMigrationCertificate(Long id) {
-        return new byte[0];
+    public byte[] generateMigrationCertificate(Long id) throws JRException {
+        String resourceDir = System.getProperty("user.dir")+"\\src\\main\\resources\\report\\";
+        Path migrationPath = Paths.get(resourceDir,"MigrationCertificate.jrxml");
+        JasperReport migrationReport = JasperCompileManager.compileReport(migrationPath.toString());
+        Migration migration = migrationRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Migration not found"));
+        JRBeanCollectionDataSource migrationDataSource = new JRBeanCollectionDataSource(Collections.singletonList(migration));
+        Map<String, Object> data = new HashMap<>();
+        for(Field field : migration.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            try {
+                data.put(field.getName(), field.get(migration));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("data", data);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(migrationReport, parameters, migrationDataSource);
+        return JasperExportManager.exportReportToPdf(jasperPrint);
     }
 
     private String saveFile(MultipartFile file) throws IOException {
